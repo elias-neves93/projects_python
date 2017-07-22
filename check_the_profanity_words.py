a1bc583f7399ee1a2da784b92a4d9ed25bb1a96e
Elias Neves <elias-neves93@hotmail.com>
2017-07-22 14:19:38 -0300

    Criado a opção de entrada de arquivo e tirar espaços e quebra linha.

--- check_the_profanity_words.py
+++ check_the_profanity_words.py
@@ -2,22 +2,19 @@ import urllib.request
 
 def read_the_file():
 
-    file = 'words' # Pegando o arquivo a ser lido!
-
-    texto = open(file) # Abrindo o arquivo e armazenando em uma variavel 'words'
-
-    arquivo = texto.read()  # Lendo o arquivo
-
-    texto.close() # Fechando o arquivo após lido!
-
-    check_the_word(arquivo)
+    escolha = input("Qual arquivo deseja verificar? \n --> ")
+    arquivo = open(escolha)
+    contents = arquivo.read()
+    arquivo.close()
+    check_the_word(contents)
 
 def check_the_word(arquivo):
 
-    real = arquivo.replace(' ','%20')
+    arquivo_tratado_sem_espaco = arquivo.replace(' ','%20')
+    arquivo_tratado_sem_quebra_linha = arquivo_tratado_sem_espaco.replace('\n', '%20')
 
     request = 'http://www.purgomalum.com/service/json?text='
-    url = request + real
+    url = request + arquivo_tratado_sem_quebra_linha
 
     request = urllib.request.urlopen(url)
 
